e5d5fa0bcaa7d195d16171128d9d10968840e027
Danielhard <335805097@qq.com>
2017-11-29 12:07:47 +0800

    ok

--- projects/xshop.sh
+++ projects/xshop.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
  
-WEB_PATH='/home/wwwroot/vnshop/'
-WEB_PATH_CLIENT='/home/wwwroot/vnshop/client'
+WEB_PATH='/home/wwwroot/myshop/'
+WEB_PATH_CLIENT='/home/wwwroot/myshop/client'
 WEB_USER='www'
 WEB_USERGROUP='www'
 # we can do 
-echo "Start deployment vx.itnote.cn"
+echo "Start deployment xshop.zzxdanielhard.xyz"
 cd $WEB_PATH
 echo "pulling source code..."
 # git reset --hard origin/release
